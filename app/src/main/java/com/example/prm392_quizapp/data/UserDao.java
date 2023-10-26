b734f1213f94994aa80c4c7e38eb46b7be9ee42d
sonn2611 <hn232324@gmail.com>
2023-10-26 09:41:12 +0700

    update Login

--- app/src/main/java/com/example/prm392_quizapp/data/UserDao.java
+++ app/src/main/java/com/example/prm392_quizapp/data/UserDao.java
@@ -10,9 +10,30 @@ import androidx.room.Update;
 
 import java.util.List;
 
-
+@Dao
 public interface UserDao {
 
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void insertUser(User user);
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAttempt(Attempt attempt);
+
+    @Update
+    void updateUser(User user);
+
+    @Query("SELECT * FROM user")
+    List<User> observeAllUser();
+
+    @Delete
+    void deleteUser(User user);
+
+    @Transaction
+    @Query("SELECT DISTINCT *  FROM attempt WHERE email = :email")
+    List<Attempt> getUserAndAttemptsWithSameEmail(String email);
+
+    @Transaction
+    @Query("SELECT SUM(earned) FROM attempt WHERE email = :email")
+    int getOverAllPoints(String email);
 
 }
--- app/src/main/java/com/example/prm392_quizapp/data/UserDatabase.java
+++ app/src/main/java/com/example/prm392_quizapp/data/UserDatabase.java
@@ -4,8 +4,13 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 
+@Database(
+        entities = {User.class,Attempt.class},
+        exportSchema = false,
+        version = 1
+)
 public abstract class UserDatabase extends RoomDatabase {
 
-
+    public abstract UserDao userDao();
 
 }
