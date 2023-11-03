dd4355a585611ee9186361ba2bbd144ace6cba4e
HoangTHHE161351 <122241506+HoangTHHE161351@users.noreply.github.com>
2023-11-03 09:45:08 +0700

    update register+history 3/11

--- app/src/main/java/com/example/prm392_quizapp/HistoryActivity.java
+++ app/src/main/java/com/example/prm392_quizapp/HistoryActivity.java
@@ -1,14 +1,19 @@
 package com.example.prm392_quizapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
-
+import com.example.prm392_quizapp.adapter.HistoryAdapter;
+import com.example.prm392_quizapp.data.Attempt;
+import com.example.prm392_quizapp.data.UserDatabase;
+import com.example.prm392_quizapp.data.UserDatabaseClient;
+import com.example.prm392_quizapp.data.UserWithAttempts;
+import com.example.prm392_quizapp.other.SharedPref;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,6 +22,77 @@ import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
 
+    private RecyclerView rvHistory;
+    private List<UserWithAttempts> userWithAttempts;
+    private TextView tvTotalPoints, tvTotalQuestions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        rvHistory = findViewById(R.id.rvHistory);
+        tvTotalQuestions = findViewById(R.id.tvTotalQuestionsHistory);
+        tvTotalPoints = findViewById(R.id.tvOverAllPointsHistory);
+
+        findViewById(R.id.imageViewHistory).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        String email = SharedPref.getInstance().getUser(this).getEmail();
+        GetAllUserAttemptTask getAllUserAttemptTask = new GetAllUserAttemptTask(email);
+        getAllUserAttemptTask.execute();
+    }
+
+
+    class GetAllUserAttemptTask extends AsyncTask<Void, Void, Void> {
+
+        private final String email;
+        ArrayList<Attempt> attempts = new ArrayList<>();
+
+        public GetAllUserAttemptTask(String email) {
+            this.email = email;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            UserDatabase databaseClient = UserDatabaseClient.getInstance(getApplicationContext());
+            attempts = (ArrayList<Attempt>) databaseClient.userDao().getUserAndAttemptsWithSameEmail(email);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            int overallPoints = 0;
+
+            for (Attempt userWithAttempts : attempts) {
+                overallPoints += userWithAttempts.getEarned();
+            }
+
+            tvTotalQuestions.setText(String.valueOf(attempts.size()));
+            tvTotalPoints.setText(String.valueOf(overallPoints));
+
+            Collections.sort(attempts, new AttemptCreatedTimeComparator());
+
+            HistoryAdapter adapter = new HistoryAdapter(attempts);
+            rvHistory.setAdapter(adapter);
+
+
+        }
+    }
+
+    public class AttemptCreatedTimeComparator implements Comparator<Attempt> {
+
+        @Override
+        public int compare(Attempt attempt, Attempt t1) {
+            return String.valueOf(t1.getCreatedTime()).compareTo(String.valueOf(attempt.getCreatedTime()));
+        }
+    }
 
 
 }
